81265b0b8f74f9cf510c88257c700d2863a1fcb4
DULA .JR <mohamedadelq17@gmail.com>
2026-02-25 02:25:24 +0200

    feat: add Vercel configuration and update BASE_URL to production endpoint

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -33,3 +33,5 @@ app.use("/cart", cartRoute);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+module.exports = app;
--- front-end/src/constants/baseUrl.ts
+++ front-end/src/constants/baseUrl.ts
@@ -1,2 +1,2 @@
 // Or use environment variables (.env)
-export const BASE_URL = "https://e-commerce-five-gamma-45.vercel.app";
+export const BASE_URL = "https://e-commerce-backend-9heq.onrender.com";
